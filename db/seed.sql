b5d5318f26cd852eb6fc7a8a015c9ade4ec243af
PESolut <jahaadpetty@pursuit.org>
2023-02-07 12:10:04 -0500

    users seed data added to seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+\c messageboard
+
+INSERT INTO users (name, email, password) VALUES
+  ('John Doe', 'johndoe1@example.com', 'password1'),
+  ('Jane Doe', 'janedoe2@example.com', 'password2'),
+  ('Jim Smith', 'jimsmith3@example.com', 'password3'),
+  ('Sara Johnson', 'sarajohnson4@example.com', 'password4'),
+  ('Mike Brown', 'mikebrown5@example.com', 'password5'),
+  ('Amy Davis', 'amydavis6@example.com', 'password6'),
+  ('Ryan Wilson', 'ryanwilson7@example.com', 'password7'),
+  ('Alex Jones', 'alexjones8@example.com', 'password8'),
+  ('Emma Clark', 'emmaclark9@example.com', 'password9'),
+  ('Tommy Davis', 'tommydavis10@example.com', 'password10');
